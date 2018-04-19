65186d8402b864ce59ad9a0cc7a71b7646f7eb41
Felipe Catania <felicatania@gmail.com>
2018-04-19 12:19:42 -0300

    init

---
+++ src/components/AgendaScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AgendaScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>This is the Agenda Screen Lord Zucc</Text>
+      </View>
+    );
+  }
+}
+
+export default AgendaScreen;
