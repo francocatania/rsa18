1597216c241ef1be1ff87719c06e11fb5dedc1e9
Felipe Catania <felicatania@gmail.com>
2018-04-20 15:27:24 -0300

    add timeline listview

--- src/components/screens/AgendaScreen.js
+++ src/components/screens/AgendaScreen.js
@@ -1,18 +1,80 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Timeline from 'react-native-timeline-listview';
+import HeaderFlightCard from '../HeaderFlightCard';
 
 class AgendaScreen extends Component {
   static navigationOptions = {
     title: 'Agenda',
   };
 
+  constructor() {
+    super();
+    this.data = [
+      {
+        type: 'text',
+        time: '09:00',
+        title: 'Arribo al Hotel Azimut',
+        description: 'Check-in en el Super Hotel 5 estrellas donde Putin estuvo con Pampita.'
+      },
+      {
+        type: 'flight',
+        time: '11:00',
+        origin: 'EZE',
+        destination: 'CDG',
+        airline: 'AirFrance',
+        flightNumber: 'AF229'
+      },
+      {
+        type: 'text',
+        time: '13:00',
+        title: 'Almuerzo con Mirtha Rusa',
+        description: 'Los invitados de hoy son Vladimir Putin, Vladimir Gayin, Vladimir Trolin.'
+      },
+      {
+        type: 'text',
+        time: '19:00',
+        title: 'Cena en la Plaza Roja',
+        description: 'Picnic con los rusos que estén paseando por ahí.'
+      }
+    ];
+  }
+
+  renderDetail(rowData, sectionId, rowId) {
+    console.log(rowData);
+    console.log(sectionId);
+    console.log(rowId);
+    if (rowData.type === 'flight') {
+      return <HeaderFlightCard {...rowData} />;
+    }
+    return <Text style={{ color: 'white', fontSize: 18, marginBottom: 15 }}>{rowData.title}</Text>;
+  }
+
   render() {
     return (
-      <View>
-        <Text>This is the Agenda Screen Lord Zucc</Text>
+      <View style={styles.pageContainer}>
+        <Timeline
+          data={this.data}
+          renderDetail={this.renderDetail.bind(this)}
+          timeStyle={styles.timeLabel}
+          innerCircle={'dot'}
+          dotColor="#333333"
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    backgroundColor: '#333333',
+    flex: 1,
+    paddingTop: 10,
+    paddingRight: 10
+  },
+  timeLabel: {
+    color: 'white'
+  }
+});
 
 export default AgendaScreen;
