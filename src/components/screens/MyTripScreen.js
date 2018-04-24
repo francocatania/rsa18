1e2ce5e2d75f964292474efedd2e67309d6e0c35
Franco Catania <catania.fco@gmail.com>
2018-04-24 17:37:45 -0300

    fix  impor to HotelScreen

--- src/components/screens/MyTripScreen.js
+++ src/components/screens/MyTripScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Dimensions } from 'react-native';
 import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
-import Hotel from '../Hotel';
+import HotelScreen from './HotelScreen';
 import FlightScreen from './FlightScreen';
  
 const initialLayout = {
@@ -9,7 +9,7 @@ const initialLayout = {
   width: Dimensions.get('window').width,
 };
  
-const FirstRoute = () => <Hotel />;
+const FirstRoute = () => <HotelScreen />;
 const SecondRoute = () => <FlightScreen />;
  
 class MyTripScreen extends Component {
