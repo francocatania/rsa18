1e4081e2863d71edad14ae9e3dc8fa007e98001a
Felipe Catania <felicatania@gmail.com>
2018-04-24 11:16:51 -0300

    change existing components to use Card

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Dimensions, StyleSheet, View, Text, Image, ScrollView, Vibration } from 'react-native';
+import { Dimensions, StyleSheet, View, Text, Image, ScrollView } from 'react-native';
+import Card from './Card';
 
 const hotelUrl = 'https://gbsn.org/wp-content/uploads/2017/03/olympic-hotel.jpg';
 const hotelUrl2 = 'https://images.trvl-media.com/hotels/1000000/10000/7800/7736/7736_195_z.jpg';
 const hotelUrl3 = 'https://t-ec.bstatic.com/images/hotel/max1024x768/328/32837688.jpg';
 
 const { width } = Dimensions.get('window');
-const Hotel = ({ 
+const Hotel = (/*{ 
   hotelName, 
   hotelNameRussian, 
   hotelImgs, 
@@ -15,29 +16,32 @@ const Hotel = ({
   hotelTelephone, 
   hotelMail, 
   hotelWebsite 
-}) => {
+}*/) => {
   const { 
     screen,
     cardContainer,
     hotelImage,
     hotelTitle,
     hotelSubtitle,
-    carousel,
-    imgContainer
-
+    carousel
   } = styles;
   
   return (
     <View style={screen}>
-      <View style={cardContainer}>
-        <ScrollView horizontal style={carousel} pagingEnabled showsHorizontalScrollIndicator={false}>
+      <Card style={cardContainer}>
+        <ScrollView 
+          horizontal
+          style={carousel}
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+        >
           <Image source={{ uri: hotelUrl }} style={hotelImage} />
           <Image source={{ uri: hotelUrl2 }} style={hotelImage} />
           <Image source={{ uri: hotelUrl3 }} style={hotelImage} />
         </ScrollView>
         <Text style={hotelTitle}>Azimut Moscow</Text>
         <Text style={hotelSubtitle}>Azefhsldkjf Mascaw</Text>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -45,22 +49,13 @@ const Hotel = ({
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    backgroundColor: '#FFFFFF'
+    backgroundColor: '#F8F8F8'
   },
   cardContainer: {
-    backgroundColor: '#F8F8F8',
-    // height: 100,
     width: '95%',
     alignSelf: 'center',
     marginTop: 10,
-    borderRadius: 4,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
-    elevation: 1,
-    marginBottom: 15,
-    overflow: 'hidden'
+    marginBottom: 15
   },
   carousel: {
     height: 300,
