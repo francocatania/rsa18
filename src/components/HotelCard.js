23092867076f2e6fc5fdf2f7f477bb1d5659ddd9
Franco Catania <catania.fco@gmail.com>
2018-04-24 19:21:45 -0300

    added phone call, website and mail functionality

--- src/components/HotelCard.js
+++ src/components/HotelCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import call from 'react-native-phone-call';
-import { Dimensions, StyleSheet, View, Text, Image, ScrollView, Linking, Button, Platform, TouchableOpacity } from 'react-native';
+import { Dimensions, StyleSheet, View, Text, Image, ScrollView, Linking, Button, Platform, TouchableOpacity, Alert } from 'react-native';
 import Card from './Card';
 
 const hotelUrl = 'https://gbsn.org/wp-content/uploads/2017/03/olympic-hotel.jpg';
@@ -37,10 +37,20 @@ const Hotel = (/*{
   const phoneCall = () => {
     call(hotelPhoneNumber).catch(console.error);
   };
-   
+
+  const sendEmail = () => {
+    Linking.openURL('mailto:foo@example.com?cc=bar@example.com&body=Escriba%20su%20mensaje%20aquí');
+  };
+  
+  const openWebsite = () => {
+    Linking.openURL('https://en.azimuthotels.com/Russia/azimut-hotel-olympic-moscow/contacts/').catch(err => console.error('An error occurred', err));
+  };
+
   const openInGoogleMaps = () => {
     Linking.openURL(`https://www.google.com/maps/search/?api=1&query=${hotelName}`);
   };
+
+  const buttonColor = Platform.OS === 'ios' ? '#FFFFFF' : '#FF5252';
 
   return (
     <Card style={cardContainer}>
@@ -58,13 +68,23 @@ const Hotel = (/*{
         <Text style={hotelTitle}>{hotelName}</Text>
         <Text style={hotelSubtitle}>Azefhsldkjf Mascaw</Text>
         <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', marginTop: 20, height: 60 }}>
-          <TouchableOpacity onPress={phoneCall}>
+          <TouchableOpacity onPress={sendEmail}>
             <View style={{ width: 50, alignItems: 'center' }}>
               <Image
-                style={styles.button}
-                source={{ uri: 'http://www.emtmarketing.com/wp-content/uploads/2016/06/phone-call-icon-16.png' }}
+                style={[styles.button, { width: 40 }]}
+                source={{ uri: 'https://www.greycon.com/wp-content/uploads/2015/09/icon-mail-light.png' }}
               />
-              <Text style={{ color: 'gray', fontSize: 12, marginTop: 5 }}>Call</Text>
+              <Text style={{ color: 'gray', fontSize: 12, marginTop: 5 }}>Mail</Text>
+            </View>
+          </TouchableOpacity>
+          <View style={{ width: 1, height: '100%', backgroundColor: 'lightgray' }} />
+          <TouchableOpacity onPress={openWebsite}>
+            <View style={{ width: 60, alignItems: 'center' }}>
+              <Image
+                style={styles.button}
+                source={{ uri: 'http://pluspng.com/img-png/website-png--600.png' }}
+              />
+              <Text style={{ color: 'gray', fontSize: 12, marginTop: 5 }}>Sitio Web</Text>
             </View>
           </TouchableOpacity>
           <View style={{ width: 1, height: '100%', backgroundColor: 'lightgray' }} />
@@ -72,27 +92,17 @@ const Hotel = (/*{
             <View style={{ width: 50, alignItems: 'center' }}>
               <Image
                 style={styles.button}
-                source={{ uri: 'http://www.emtmarketing.com/wp-content/uploads/2016/06/phone-call-icon-16.png' }}
+                source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnQDrmixVg0fhaeSpqQ7vPakcc6u77kK6msdPUwvDYTEmENMm6' }}
               />
-              <Text style={{ color: 'gray', fontSize: 12, marginTop: 5 }}>Call</Text>
-            </View>
-          </TouchableOpacity>
-          <View style={{ width: 1, height: '100%', backgroundColor: 'lightgray' }} />
-          <TouchableOpacity onPress={phoneCall}>
-            <View style={{ width: 50, alignItems: 'center' }}>
-              <Image
-                style={styles.button}
-                source={{ uri: 'http://www.emtmarketing.com/wp-content/uploads/2016/06/phone-call-icon-16.png' }}
-              />
-              <Text style={{ color: 'gray', fontSize: 12, marginTop: 5 }}>Call</Text>
+              <Text style={{ color: 'gray', fontSize: 12, marginTop: 5 }}>Llamar</Text>
             </View>
           </TouchableOpacity>
         </View>
         <View style={buttonContainer}>
           <Button
             onPress={openInGoogleMaps}
-            title="Open in Google Maps"
-            color='#FF5252'
+            title="Abrir en Google Maps"
+            color={buttonColor}
           />
         </View>
       </View>
@@ -128,8 +138,7 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     marginTop: 20,
-    alignSelf: Platform.OS === 'ios' ? 'flex-start' : undefined,
-    backgroundColor: 'lightblue',
+    backgroundColor: Platform.OS === 'ios' ? '#FF5252' : 'transparent',
   },
   button: {
     width: 30,
