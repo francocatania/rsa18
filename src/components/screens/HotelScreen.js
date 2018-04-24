f95914a29ac53b8f72b9e57f31a6d3cbfced604a
Franco Catania <catania.fco@gmail.com>
2018-04-24 17:38:09 -0300

    add HotelCard + basic styling

---
+++ src/components/screens/HotelScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import HotelCard from '../HotelCard';
+
+const HotelScreen = () => (
+    <ScrollView>
+      <View style={styles.pageContainer}>
+        <HotelCard />
+        <HotelCard />
+      </View>
+    </ScrollView>
+);
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    padding: 10,
+    backgroundColor: '#F8F8F8'
+  }
+});
+
+export default HotelScreen;
