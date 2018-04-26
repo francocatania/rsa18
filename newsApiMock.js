24160e3d29b61ebbef1eefe76f56b6424769240a
Franco Catania <catania.fco@gmail.com>
2018-04-26 20:10:58 -0300

    changed source to Infobae

--- newsApiMock.js
+++ newsApiMock.js
@@ -1,28 +1,40 @@
 const apiResponse = [
   {
-      source: {
-          id: 'la-nacion',
-          name: 'La Nacion'
-      },
-      author: 'lanacion.com',
-      title: 'Mundial Rusia 2018: el divertido mensaje del entrenador de la selección de Islandia para Lionel Messi',
-      description: '',
-      url: 'https://www.lanacion.com.ar/2112333-el-simpatico-mensaje-del-entrenador-de-la-seleccion-de-islandia-para-lionel-messi-previo-al-debut-en-el-mundial-rusia-2018',
-      urlToImage: '//bucket3.glanacion.com/anexos/fotos/09/2638209.png',
-      publishedAt: '2018-02-26T18:47:00Z'
-  },
-  {
-      source: {
-          id: 'la-nacion',
-          name: 'La Nacion'
-      },
-      author: 'lanacion.com',
-      title: 'Con Lionel Messi como modelo se presentó la remera suplente: en detalle, la nueva camiseta para el Mundial Rusia 2018',
-      description: '',
-      url: 'https://www.lanacion.com.ar/2118552-con-lionel-messi-como-modelo-se-presento-la-remera-suplente-en-detalle-la-nueva-camiseta-para-el-mundial-rusia-2018',
-      urlToImage: '//bucket1.glanacion.com/anexos/fotos/41/2651141.jpg',
-      publishedAt: '2018-03-20T12:20:00Z'
-  }
+    "source": {
+        "id": "infobae",
+        "name": "Infobae"
+    },
+    "author": null,
+    "title": "El jugador de Boca que deslumbró a Victoria Lopyreva, la embajadora del Mundial de Rusia - Infobae.com",
+    "description": "Infobae.com El jugador de Boca que deslumbró a Victoria Lopyreva, la embajadora del Mundial de Rusia Infobae.com En el contexto de su visita a la Argentina, Victoria Lopyreva, la embajadora del Mundial de Rusia, brindó una conferencia de prensa en el complejo…",
+    "url": "https://www.infobae.com/deportes-2/mundial-2018/2018/02/12/el-jugador-de-boca-que-deslumbro-a-victoria-lopyreva-la-embjadora-del-mundial-de-rusia-pavon/",
+    "urlToImage": "https://www.infobae.com/new-resizer/-K41FBdijBhX8JRzXUK7zfsYp-8=/1200x0/filters:quality(100)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/02/12165500/IMGM2262.jpg",
+    "publishedAt": "2018-02-12T19:28:41Z"
+},
+{
+    "source": {
+        "id": "infobae",
+        "name": "Infobae"
+    },
+    "author": null,
+    "title": "Los tres goles de Lautaro Martínez ante la mirada de Sampaoli: ¿Sacó pasaje al Mundial Rusia 2018? - Infobae.com",
+    "description": "Infobae.com Los tres goles de Lautaro Martínez ante la mirada de Sampaoli: ¿Sacó pasaje al Mundial Rusia 2018? Infobae.com El delantero de Racing aprovechó la visita del DT de la Selección al Cilindro y fue la gran figura de la goleada a Huracán. Su desempeño…",
+    "url": "https://www.infobae.com/deportes-2/2018/02/05/los-tres-goles-de-lautaro-martinez-ante-la-mirada-de-sampaoli-saco-pasaje-al-mundial-rusia-2018/",
+    "urlToImage": "https://www.infobae.com/new-resizer/kcIg86By-9X0aMJU4V1udUAVA_A=/1200x0/filters:quality(100)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/02/05074130/lautaro-martinez-celebra-racing-vs-huracan-1920.jpg",
+    "publishedAt": "2018-02-05T11:39:52Z"
+},
+{
+    "source": {
+        "id": "infobae",
+        "name": "Infobae"
+    },
+    "author": null,
+    "title": "\"Passarella me cagó un Mundial\" y otras 19 contundentes frases de Claudio Paul Caniggia - Infobae.com",
+    "description": "Infobae.com \"Passarella me cagó un Mundial\" y otras 19 contundentes frases de Claudio Paul Caniggia Infobae.com Claudio Paul Caniggia, de visita en el país, brindó una extensa entrevista al programa Fútbol 910, que conduce Toti Pasman por Radio La Red. El \"Pá…",
+    "url": "https://www.infobae.com/deportes-2/2018/04/24/passarella-me-cago-un-mundial-y-otras-19-contundentes-frases-de-claudio-paul-caniggia/",
+    "urlToImage": "https://www.infobae.com/new-resizer/HAHkjV0T29PTw-vmbdO5sXrzw78=/1200x0/filters:quality(100)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2017/12/06213253/Claudio-Paul-Caniggia-2.jpg",
+    "publishedAt": "2018-04-24T05:19:19Z"
+}
 ];
 
 export default apiResponse;
