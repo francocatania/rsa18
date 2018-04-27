1c52496bfa6af4d08adfd685cbd5819a0acea073
Felipe Catania <felicatania@gmail.com>
2018-04-27 17:32:13 -0300

    fetch hotel data from firebase

--- src/components/screens/HotelScreen.js
+++ src/components/screens/HotelScreen.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, ScrollView, AsyncStorage, ActivityIndicator } from 'react-native';
+import firebase from 'firebase';
 import HotelCard from '../HotelCard';
 
-const HotelScreen = () => (
-    <ScrollView>
-      <View style={styles.pageContainer}>
-        <HotelCard />
-        <HotelCard />
+class HotelScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hotels: null
+    };
+  }
+
+  componentDidMount() {
+    AsyncStorage.getItem('loginCode').then(response => {
+      firebase.database().ref(`${response}/hotels`)
+        .once('value', snapshot => {
+          this.setState({ hotels: snapshot.val() });
+        });
+    });
+  }
+
+  renderSpinner() {
+    return (
+      <View style={styles.spinnerContainer}>
+        <ActivityIndicator size="large" style={{ alignSelf: 'center' }} />
       </View>
-    </ScrollView>
-);
+    );
+  }
+
+  renderHotels() {
+    return (
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.pageContainer}>
+          {this.state.hotels.map((hotel, idx) => <HotelCard {...hotel} key={idx} />)}
+        </View>
+      </ScrollView>
+    );
+  }
+
+  render() {
+    return this.state.hotels ? this.renderHotels() : this.renderSpinner();
+  }
+}
 
 const styles = StyleSheet.create({
+  spinnerContainer: {
+    flex: 1,
+    justifyContent: 'center'
+  },
   pageContainer: {
     padding: 10,
   }
