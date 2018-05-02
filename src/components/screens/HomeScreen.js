5e44d897cb1009f89a589f8cd909aa60e6a57e4a
Franco Catania <catania.fco@gmail.com>
2018-05-02 17:59:45 -0300

    render MatchCard in Home

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, ActivityIndicator } from 'react-native';
+import firebase from 'firebase';
 import NewsList from '../NewsList';
+import MatchCard from '../MatchCard';
 
 class HomeScreen extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      match: null,
+      news: null
+    };
+  }
+
+  componentDidMount() {
+    firebase.database().ref('home')
+      .once('value', snapshot => {
+        this.setState({ match: snapshot.val().match, news: snapshot.val().news });
+      });
+  }
+
+  renderSpinner() {
+    return (
+      <View style={styles.spinnerContainer}>
+        <ActivityIndicator size="large" style={{ alignSelf: 'center' }} />
+      </View>
+    );
+  }
+
+  renderHome() {
     const patagonikLogoURL = 'https://firebasestorage.googleapis.com/v0/b/autho-c54b4.appspot.com/o/patagonik_logo.png?alt=media&token=70c96e9f-2835-49a2-9c62-c3443f502e70';
     const masterLogoURL = 'http://adsynergy.co.uk/wp-content/uploads/2017/06/mastercard-logo1.png';
-
     return (
       <ScrollView>
         <View style={styles.pageContainer}>
@@ -20,15 +44,25 @@ class HomeScreen extends Component {
               style={[styles.logoImage, { resizeMode: 'contain' }]}
             />
           </View>
+          <Text style={styles.title}>Próximo partido</Text>
+          <MatchCard {...this.state.match} />
           <Text style={styles.title}>Noticias</Text>
           <NewsList />
         </View>
       </ScrollView>
     );
   }
+
+  render() {
+    return this.state.match && this.state.news ? this.renderHome() : this.renderSpinner(); 
+  }
 }
 
 const styles = StyleSheet.create({
+  spinnerContainer: {
+    flex: 1,
+    justifyContent: 'center'
+  },
   pageContainer: {
     flex: 1,
     padding: 10
